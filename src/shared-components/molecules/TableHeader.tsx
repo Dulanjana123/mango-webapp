517a3b66fa345b973ea971d5a6fabed91db60b99
Dulanjana Dissanayake <dulanjana.dissanayake@codicetech.com>
2024-12-02 20:58:14 +0530

    feature : Implement ATOMs for Login form and Order list. Implement Authentication for order service.

---
+++ src/shared-components/molecules/TableHeader.tsx
@@ -0,0 +1,16 @@
+function TableHeader() {
+  return (
+    <div className="table-header">
+      <div className="table-header__cell">ID</div>
+      <div className="table-header__cell">Name</div>
+      <div className="table-header__cell">Phone</div>
+      <div className="table-header__cell">Total</div>
+      <div className="table-header__cell">Items</div>
+      <div className="table-header__cell">Date</div>
+      <div className="table-header__cell"></div>
+    </div>
+  );
+}
+
+export default TableHeader;
+
